0b499b374f5ef21eabe8ce017cd6a9ed5ad79626
Kreverson <kreversonjulho@gmail.com>
2025-02-18 18:44:43 -0300

    Melhorado para retornar apenas informações relevantes da paginação

--- src/main/java/artapp/order_integration/controller/OrderController.java
+++ src/main/java/artapp/order_integration/controller/OrderController.java
@@ -1,16 +1,15 @@
 package artapp.order_integration.controller;
 
+import artapp.order_integration.controller.dto.ApiResponse;
 import artapp.order_integration.controller.dto.OrderResponse;
+import artapp.order_integration.controller.dto.PaginationResponse;
 import artapp.order_integration.service.OrderService;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class OrderController {
@@ -27,10 +26,15 @@ public class OrderController {
     }
 
     @GetMapping("/customer/{customerId}/orders")
-    public ResponseEntity<Page<OrderResponse>> getAllOrderByCustomer(@PathVariable("customerId") Long customerId,
+    public ResponseEntity<ApiResponse<OrderResponse>> getAllOrderByCustomer(@PathVariable("customerId") Long customerId,
                                                                      @RequestParam(name = "page", defaultValue = "0") Integer page,
                                                                      @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
 
-        return ResponseEntity.ok(orderService.findAllByCustomerId(customerId, PageRequest.of(page, pageSize)));
+        var pageResponse = orderService.findAllByCustomerId(customerId, PageRequest.of(page, pageSize));
+
+        return ResponseEntity.ok(
+                new ApiResponse<>(
+                        pageResponse.getContent(),
+                        PaginationResponse.fromPage(pageResponse)));
     }
 }
