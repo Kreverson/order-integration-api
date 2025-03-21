69843a628127fff9a525a1bf8ea89b88078bc192
kreverson <kreversonjulho@gmail.com>
2025-03-21 01:39:50 -0300

    Adicionado Grafana no docker compose e bean no spring boot

--- src/main/java/artapp/order_integration/OrderIntegrationApplication.java
+++ src/main/java/artapp/order_integration/OrderIntegrationApplication.java
@@ -1,7 +1,11 @@
 package artapp.order_integration;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 
 @SpringBootApplication
 public class OrderIntegrationApplication {
@@ -10,4 +14,8 @@ public class OrderIntegrationApplication {
 		SpringApplication.run(OrderIntegrationApplication.class, args);
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
